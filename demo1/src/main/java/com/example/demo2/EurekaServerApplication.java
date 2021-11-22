4ed94ebd6f3e725ff508fee6829b7a13af4a5127
zhaoxiaotao601 <1849061898@qq.com>
2021-11-22 13:42:56 +0800

    第一个cloud

---
+++ demo1/src/main/java/com/example/demo2/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo2;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: PC
+ * \* Date: 2021/11/22
+ * \* Time: 12:46
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public class EurekaServerApplication {
+}
